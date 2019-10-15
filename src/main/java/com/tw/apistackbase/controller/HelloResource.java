cc304d6db5a44bdadf06541a66a7e178867a18f5
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-15 20:10:27 +0800

    create method to add employee

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,18 +1,23 @@
 package com.tw.apistackbase.controller;
 
+import com.tw.apistackbase.model.Employee;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloResource {
 
+    private List<Employee> employeeList = new ArrayList<>();
     private final Logger log = Logger.getLogger(this.getClass().getName());
+
 
     @GetMapping(path = "/{userName}", produces = {"application/json"})
     public ResponseEntity<String> getAll(@PathVariable String userName) {
@@ -20,17 +25,17 @@ public class HelloResource {
         return ResponseEntity.ok("Hello:" + userName);
     }
 
-    @ResponseBody
-    @GetMapping(path = "/test-response-body/{name}", produces = {"application/json"})
-    public ResponseTransfer postResponseController(@PathVariable String name) {
-        return new ResponseTransfer("Thanks "+ name +" For Posting!!!");
-    }
+//    @ResponseBody
+//    @GetMapping(path = "/test-response-body/{name}", produces = {"application/json"})
+//    public ResponseTransfer postResponseController (
+//            @RequestBody LoginForm loginForm) {
+//        return new ResponseTransfer("Thanks "+ loginForm.getUserName() +" For Requesting!!!");
+//    }
 
-    @PostMapping("/test-request-body")
-    public ResponseTransfer requestController(
-            @RequestBody LoginForm loginForm) {
-        return new ResponseTransfer("Thanks "+ loginForm.getUserName() +" For Requesting!!!");
+    @PostMapping(path = "/create-employee", consumes = "application/json", produces = "application/json")
+    public List<Employee> createEmployee(@RequestBody Employee employee) {
+        employeeList.add(employee);
+        return  employeeList;
     }
-
 
 }
